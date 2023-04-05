13cc13bc3ce37705f003a45539b8947466e6e08b
SriSaiKrishnan <krishnansai99@gmail.com>
2023-04-05 09:30:34 +0530

    Recursion Practice

---
+++ src/com/sai/recursion/Palindrome.java
@@ -0,0 +1,2 @@
+package com.sai.recursion;public class Palindrome {
+}
--- src/com/sai/recursion/PrintNtoOne.java
+++ src/com/sai/recursion/PrintNtoOne.java
@@ -1,2 +1,17 @@
-package com.sai.recursion;public class PrintNtoOne {
+package com.sai.recursion;
+
+public class PrintNtoOne {
+
+    public static void main(String[] args) {
+        int i=1;
+        int n=10;
+        printNToOne(i,n);
+    }
+
+    private static void printNToOne(int i, int n) {
+        System.out.println(n);
+        if(n>i)
+            printNToOne(i,n-1);
+    }
+
 }
