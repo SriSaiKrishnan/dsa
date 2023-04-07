993c584e64374704fc98353e59b80969ee54d322
SriSaiKrishnan <krishnansai99@gmail.com>
2023-04-07 09:40:32 +0530

    Recursion Practice

---
+++ src/com/sai/array/Anagram.java
@@ -0,0 +1,29 @@
+package com.sai.recursion;
+
+public class Anagram {
+
+        public static void main(String[] args) {
+                System.out.println(anag());
+        }
+
+public static boolean anag() {
+
+        String s = "acc", t = "abc";
+
+        if(s.length() != t.length()) return false;
+        int[] arr = new int[26];
+
+        for(Character st : s.toCharArray()){
+        arr[st - 'a']++;
+        }
+
+        for(Character st : t.toCharArray()){
+
+        if(arr[st - 'a'] <=0) return false;
+        else
+        arr[st - 'a']--;
+        }
+
+        return true;
+        }
+}
--- src/com/sai/recursion/Palindrome.java
+++ src/com/sai/recursion/Palindrome.java
@@ -1,2 +1,45 @@
-package com.sai.recursion;public class Palindrome {
+package com.sai.recursion;
+
+/*
+
+Problem Statement: “Given a string, check if the string is palindrome or not.”  A string is said to be palindrome if the reverse of the string is the same as the string.
+
+Examples:
+
+Example 1:
+Input: Str =  “ABCDCBA”
+Output: Palindrome
+Explanation: String when reversed is the same as string.
+
+Example 2:
+Input: Str = “TAKE U FORWARD”
+Output: Not Palindrome
+Explanation: String when reversed is not the same as string.
+
+ */
+
+public class Palindrome {
+
+    public static String  reversedStr = "";
+
+    public static void main(String[] args) {
+        String str = "TAKE U FORWARD";
+        int len = str.length();
+       if(str.equalsIgnoreCase(palindrome(str,len)))
+           System.out.println("The String is Palindrome");
+       else
+           System.out.println("The String is not Palindrome");
+    }
+
+    private static String palindrome(String str, int len) {
+
+
+        if(len<=0)
+            return reversedStr;
+
+        reversedStr = reversedStr + str.substring(len-1, len);
+        return  palindrome(str,--len);
+
+    }
+
 }
