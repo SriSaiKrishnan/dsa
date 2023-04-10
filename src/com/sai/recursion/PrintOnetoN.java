602bba707f3b634295cb076d76e5c6475270fa23
SriSaiKrishnan <krishnansai99@gmail.com>
2023-04-10 18:04:04 +0530

    Recursion Practice

--- src/com/sai/recursion/PrintOnetoN.java
+++ src/com/sai/recursion/PrintOnetoN.java
@@ -1,2 +1,23 @@
-package com.sai.recursion;public class PrintOnetoN {
+package com.sai.recursion;
+
+public class PrintOnetoN {
+
+    /*
+        Print from 1 to N using Recursion
+     */
+
+    public static void main(String[] args) {
+        int i=1;
+        int n=10;
+        printOneToN(i,10);
+    }
+
+    private static void printOneToN(int i, int n) {
+
+        System.out.println(i);
+        if(i<n)
+            printOneToN(i+1,n);
+
+    }
+
 }
