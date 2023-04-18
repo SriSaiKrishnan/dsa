ba3b8791c05eca5c67c85126369aecc4c4b36983
SriSaiKrishnan <krishnansai99@gmail.com>
2023-04-18 08:49:46 +0530

    Merge Sort Practice

---
+++ src/com/sai/sorting/MergeSort.java
@@ -0,0 +1,57 @@
+package com.sai.sorting;
+
+public class MergeSort {
+
+    public static int arr[] = { 10, 7, 1, 8, 16, 12 };
+
+    public static void main(String[] args) {
+        int j = arr.length;
+        mergeSort(arr,j);
+        for (int i=0; i< a.length; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+}
