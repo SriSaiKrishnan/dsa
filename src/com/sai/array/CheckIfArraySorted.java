81a5ea3ee8208150b62c63079a5fa9c1e5cccb57
SriSaiKrishnan <krishnansai99@gmail.com>
2023-04-24 11:13:56 +0530

    Array Practice

--- src/com/sai/array/CheckIfArraySorted.java
+++ src/com/sai/array/CheckIfArraySorted.java
@@ -1,2 +1,32 @@
-package com.sai.array;public class CheckIfArraySorted {
+package com.sai.array;
+
+/*
+
+Examples:
+
+Example 1:
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: True.
+Explanation: The given array is sorted i.e Every element in the array is smaller than or equals to its next values, So the answer is True.
+
+ */
+
+public class CheckIfArraySorted {
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        System.out.println(checkSorted(arr));
+    }
+
+    private static boolean checkSorted(int[] arr) {
+
+        for (int i=1; i<arr.length; i++)
+        {
+            if(arr[i]<arr[i-1])
+                return false;
+        }
+        return true;
+
+    }
+
 }
