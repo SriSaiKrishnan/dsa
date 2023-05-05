a06b116de9ab3295b87009aa07f321b5c7ea4314
SriSaiKrishnan <krishnansai99@gmail.com>
2023-05-05 19:42:44 +0530

    Array Practice

--- src/com/sai/array/SingleElement.java
+++ src/com/sai/array/SingleElement.java
@@ -1,2 +1,44 @@
-package com.sai.array;public class SingleElement {
+package com.sai.array;
+
+/*
+
+Problem Statement: Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
+
+Examples:
+
+Example 1:
+Input Format: arr[] = {2,2,1}
+Result: 1
+Explanation: In this array, only the element 1 appear once and so it is the answer.
+
+ */
+
+public class SingleElement {
+
+    public static void main(String[] args) {
+        int arr[] = {2,3,2,5,1,1,3};
+        System.out.println(singleNumber(arr));
+    }
+
+    private static int singleNumber(int[] arr) {
+
+        int ar[] = new int[arr.length];
+        int num = 0;
+
+        for (int i=0; i<arr.length; i++)
+        {
+           ar[arr[i]] = ar[arr[i]] +1;
+            if (ar[arr[i]] == 1)
+                num = arr[i];
+            }
+        for (int i=0; i<ar.length; i++)
+        {
+            if(ar[i] == 1)
+                num = i;
+        }
+        return num;
+    }
+
+
+
 }
