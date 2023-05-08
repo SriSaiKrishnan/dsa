8718835cff20b68fde31e24738477c59ae1a9fec
SriSaiKrishnan <krishnansai99@gmail.com>
2023-05-08 11:18:38 +0530

    Array Practice

--- src/com/sai/array/LongestSubarray.java
+++ src/com/sai/array/LongestSubarray.java
@@ -1,2 +1,56 @@
-package com.sai.array;public class LongestSubarray {
+package com.sai.array;
+
+/*
+
+Problem Statement: Given an array and a sum k, we need to print the length of the longest subarray that sums to k.
+
+Examples:
+
+Example 1:
+Input Format: N = 3, k = 5, array[] = {2,3,5}
+Result: 2
+Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
+
+Example 2:
+Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+Result: 3
+Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+
+ */
+
+public class LongestSubarray {
+
+    public static void main(String[] args) {
+        int arr[] = {2,3,5,1,9};
+        int k = 10;
+        System.out.println(getLongestSubarray(arr,k));
+    }
+
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
 }
