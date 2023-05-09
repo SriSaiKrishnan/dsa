e56a79eb5cb4b8068b99ddad2277e2f717e2f584
SriSaiKrishnan <krishnansai99@gmail.com>
2023-05-09 09:02:54 +0530

    Array Practice

--- src/com/sai/array/LeadersInArray.java
+++ src/com/sai/array/LeadersInArray.java
@@ -1,2 +1,40 @@
-package com.sai.array;public class LeadersInArray {
+package com.sai.array;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LeadersInArray {
+
+    public static void main(String[] args) {
+        int arr[] = {4, 7, 1, 0};
+        leaderInArray(arr);
+    }
+
+    private static void leaderInArray(int[] arr) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i=0; i<arr.length-1; i++)
+        {
+            boolean great = true;
+
+            for (int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    great = false;
+                    break;
+                }
+            }
+            if(great)
+            {
+                set.add(arr[i]);
+            }
+            set.add(arr[arr.length-1]);
+        }
+        System.out.println(set);
+    }
+
 }
