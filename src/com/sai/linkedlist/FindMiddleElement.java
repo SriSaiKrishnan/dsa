45ed7e77e3a8e1bf32b45758a8aed38663855813
SriSaiKrishnan <krishnansai99@gmail.com>
2023-07-20 09:18:35 +0530

    Find Middle Element in Linked List

--- src/com/sai/linkedlist/FindMiddleElement.java
+++ src/com/sai/linkedlist/FindMiddleElement.java
@@ -1,5 +1,22 @@
 package com.sai.linkedlist;
 
+/*
+
+Problem Statement: Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
+
+Examples:
+
+Input Format:
+( Pointer / Access to the head of a Linked list )
+head = [1,2,3,4,5]
+
+Result: [3,4,5]
+( As we will return the middle of Linked list the further linked list will be still available )
+
+Explanation: The middle node of the list is node 3 as in the below image.
+
+ */
+
 
     class ListNodee {
 
