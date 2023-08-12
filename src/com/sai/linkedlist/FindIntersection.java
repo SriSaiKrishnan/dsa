388b49fde626c3f7b0654f0ac87351d123615b6c
SriSaiKrishnan <krishnansai99@gmail.com>
2023-08-12 14:22:15 +0530

    Find intersection point in the linked list

--- src/com/sai/linkedlist/FindIntersection.java
+++ src/com/sai/linkedlist/FindIntersection.java
@@ -1,5 +1,20 @@
 package com.sai.linkedlist;
 
+/*
+
+Problem Statement: Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+Examples:
+
+Example 1:
+Input:
+List 1 = [1,3,1,2,4], List 2 = [3,2,4]
+Output:
+2
+Explanation: Here, both lists intersecting nodes start from node 2.
+
+ */
+
 import java.util.HashSet;
 
 public class FindIntersection {
